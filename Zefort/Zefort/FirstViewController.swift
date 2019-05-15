148eaa46eef4dd3c582c62222802fa5219280a09
Nik Fikri <nikfikrics@gmail.com>
2019-05-16 00:18:55 +0800

    Add files via upload

---
+++ Zefort/Zefort/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Zefort
+//
+//  Created by NIK FIKRI on 19/04/2019.
+//  Copyright © 2019 Zyepher. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Zefort/Zefort/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Zefort
+//
+//  Created by NIK FIKRI on 19/04/2019.
+//  Copyright © 2019 Zyepher. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
